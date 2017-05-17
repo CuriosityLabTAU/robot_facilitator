73d69b83455fbb9192629172d6f9fb3aa90f92f1
gorengordon <goren@gorengordon.com>
2017-05-17 14:44:52 +0300

    rinat made changes to kivi

--- robotator_app/robot_facilitator_app.py
+++ robotator_app/robot_facilitator_app.py
@@ -2,13 +2,19 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.checkbox import CheckBox
 import random
 
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 import random
+
+
+class MyScreenManager (ScreenManager):
+    the_tablet = None
 
 class ScatterTextWidget (BoxLayout):
     def change_label_color(self, *args):
@@ -22,9 +28,16 @@ class ScatterTextWidget (BoxLayout):
         label2.color = color
         print(label1.color)
 
+    def on_checkbox_active(self, checkbox, value):
+        if value:
+            print('The checkbox', checkbox, 'is active')
+        else:
+            print('The checkbox', checkbox, 'is inactive')
 class RobotatorApp(App):  #The name of the class will make it search for learning2.kv
     def build(self):
-        return ScatterTextWidget ()
+        #return ScatterTextWidget ()
+        self.screen_manager = MyScreenManager()
+
 
 if __name__ == "__main__":
     RobotatorApp().run()
